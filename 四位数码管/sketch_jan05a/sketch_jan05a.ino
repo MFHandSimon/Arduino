6db95eb59b732b1642ffe91a8d137118d854c86c
mfh <3535237184@qq.com>
2025-01-20 17:19:41 +0800

    Update sketch_jan05a.ino

--- 四位数码管/sketch_jan05a/sketch_jan05a.ino
+++ 四位数码管/sketch_jan05a/sketch_jan05a.ino
@@ -1,4 +1,4 @@
-void specialDigitDispaly(byte digit, byte num) {
+void specialDigitDisplay(byte digit, byte num) {
   byte val = 0;
   val = bitSet(val, digit - 1);
   shiftOut(dataPin, clockPin, MSBFIRST, val);   //将一个字节（8 位）的数据按照指定的顺序和时序，从一个数据引脚逐位输出到另一个时钟引脚
